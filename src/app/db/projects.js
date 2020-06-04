81e52dab75429ed2f709adf69c44565479a9c070
74nguyentan <exfick@gmail.com>
2020-06-04 21:25:24 +0700

    add db project and product

---
+++ src/app/db/projects.js
@@ -0,0 +1,24 @@
+[
+    {
+        "idProject": "1",
+        "nameProject": "chung cư Fx",
+        "address": "256 Hải Bắc",
+        "acreage": "5 ha",
+        "price": "500",
+        "startDay": "25/03/2020",
+        "stopDay": "12/09/2021",
+        "status" : "đã hoàn thành",
+        "image": "hinh1.jpg"
+    },
+    {
+      "idProject": "2",
+      "nameProject": "chung cư ABC",
+      "address": "246 Hải Châu",
+      "acreage": "10 ha",
+      "price": "500",
+      "startDay": "03/03/2020",
+      "stopDay": "12/06/2022",
+      "status" : "đã hoàn thành",
+      "image": "hinh2.jpg"
+  },
+  ]
