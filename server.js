06922aaddd17a6af100ad5d1a3af36d8936fd910
Truong The Cong <truongthecong010499@gmail.com>
2020-06-16 15:08:26 +0700

    kak

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path =  require('path');
 const app = express();
 app.use(express.stactic(__dirname + '/dist/fxTeam'));
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
 res.sendFile(path.join(__dirname + '/dist/fxTeam/index.html'));
 });
 app.listen(process.env.PORT || 8080);
