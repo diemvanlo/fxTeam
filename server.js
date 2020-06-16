cc872b5881d7ae429bae94be6ef92b61b7a064fb
Truong The Cong <truongthecong010499@gmail.com>
2020-06-16 15:02:07 +0700

    fuck

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path =  require('path');
 const app = express();
-app.use(express().stactic(__dirname + '/dist/fxTeam'));
-app.listen(process.env.PORT || 8080);
+app.use(express.stactic(__dirname + '/dist/fxTeam'));
+
 app.get('/*', function (req, res) {
 res.sendfile(path.join(__dirname + '/dist/fxTeam/index.html'));
-})
-console.log('console listening!')
+});
+app.listen(process.env.PORT || 8080);
+
