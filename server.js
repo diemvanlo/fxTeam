1299ea76fe2f134a9f30bbef618d96f9b2ac360a
Truong The Cong <truongthecong010499@gmail.com>
2020-06-17 13:20:52 +0700

    better

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const path =  require('path');
-const app = express();
-app.use(express.stactic(__dirname + '/dist/fxTeam'));
+const path = require('path');
 
-app.get('*', function (req, res) {
-res.sendFile(path.join(__dirname + '/dist/fxTeam/index.html'));
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/pi-project'));
+
+app.get('/*', function(req,res) {
+
+  if(req.url === '/') {
+    res.sendFile(path.join(__dirname,'/dist/fxTeam/index.html'));
+  }
+  else {
+    res.sendFile(path.join(__dirname, '/dist/fxTeam' + req.url))
+  }
+
 });
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
